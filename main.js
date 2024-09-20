dbd3e8248e85743ce307d26a10db11f81bf84a49
KelvynCipriano <133382331+KelvynCipriano@users.noreply.github.com>
2024-09-20 12:12:22 -0300

    20/09/2024

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("Banana");
+console.log("banana");
+
+var titulo = document.querySelector("h1");
+titulo.textContent = "Banana Nutrição";
+console.log(titulo);
+
+var trPaulo = document.querySelector("#paulo");
+console.log(trPaulo);
+
+var tdPeso = trPaulo.querySelector(".info-peso");
+tdPeso.textContent = "150";
