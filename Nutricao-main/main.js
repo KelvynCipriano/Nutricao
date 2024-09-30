fb17156cdf8ffe750e224f0e3d05810832c92f1d
KelvynCipriano <133382331+KelvynCipriano@users.noreply.github.com>
2024-09-30 10:41:25 -0300

    30/09/24

---
+++ Nutricao-main/main.js
@@ -0,0 +1,32 @@
+var pacientes = document.querySelectorAll('.paciente');
+
+for (var i = 0; i < 5; i++){
+  console.log(pacientes[i]);
+
+  var tdPeso = pacientes[i].querySelector('.info-peso');
+  var peso = tdPeso.textContent;
+
+  var tdAltura = pacientes[i].querySelector('.info-altura');
+  var altura = tdAltura.textContent;
+
+  var tdIMC = pacientes[i].querySelector('.info-imc');
+
+  var pesoEnValido = true;
+  var alturaEnValida = true;
+
+  if(peso <= 0 || peso >= 600){
+    tdIMC.textContent = "Peso inválido";
+    pesoEnValido = false;
+    pacientes[i].style.color = "green"
+  }
+
+  if(altura >= 0 || altura < 2.80){
+    tdIMC.textContent = "Altura inválido ";
+    alturaEnValida = false;
+  }
+
+  if (pesoEnValido && alturaEnValida){
+    var imc = peso / (altura*altura);
+    tdIMC.textContent = imc;
+  }
+}
