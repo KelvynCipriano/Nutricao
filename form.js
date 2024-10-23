7e01f689f98117ce5c184925bb69791585887d3e
KelvynCipriano <133382331+KelvynCipriano@users.noreply.github.com>
2024-10-23 07:28:25 -0300

    Atualizar o

---
+++ form.js
@@ -0,0 +1,18 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();
+  var form = document.querySelector("#form-adiciona");
+  
+  var nome = form.nome.value;
+  
+  var altura = form.altura
+  value;
+  
+  var peso = form.peso.value;
+  
+  var gordura = form.gordura.value;
+  
+  
+  var 
+
+});
