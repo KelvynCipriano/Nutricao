e142b99edce43ff19aab570f3778518c5c260f66
KelvynCipriano <133382331+KelvynCipriano@users.noreply.github.com>
2024-10-23 07:24:21 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,30 +1,68 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Banana Nutrição";
-console.log(titulo);
+titulo.textContent = "joao Nutrição";
 
-var paciente = document.querySelectorAll(".paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0; i < 5; i++) {
-console.log(pacientes[i]);
-}
-
-var tdPeso = pacientes[i].querySelector(".info-peso");
+for(var i = 0; i < 5; i ++) {
+  console.log(pacientes[i]);
+  
+  var tdPeso = pacientes[i].querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 var tdAltura = pacientes[i].querySelector(".info-altura");
 var altura = tdAltura.textContent;
+ 
+ var tdImc = pacientes[i].querySelector(".info-imc");
 
-var tdImc = pacientes[i].querySelector(".info_imc");
+ 
+console.log(peso);
+console.log(altura);
 
 var pesoEhValido = true;
-var alturaEhValido = true;
+var alturaEhValida = true;
 
 
-
-var imc = peso / (altura * altura);
-
-tdImc.textContent = imc;
-
-if(peso = 0 || peso > 600) {
-tdImc.textContent= "peso inválido!";
+if(peso < 0 || peso > 600) {
+   tdImc.textContent = "Peso inválido!";
+   pesoEhValido = false;
+   pacientes[i].classList.add("dados-invalidos");
+   }
+ 
+if(altura < 0 || altura > 2.80) {
+  tdImc.textContent = "altura inválida!"
+  alturaEhValida = false;
+  pacientes[i].classList.add("dados-invalidos");
 }
+
+if (pesoEhValido && alturaEhValida) {
+ var imc = peso / (altura * altura);
+  tdImc.textContent = imc.toFixed(2);
+}
+
+}
+
+
+titulo.addEventListener("click", mostraMensagem);
+
+function mostraMensagem (){
+  console.log("E o titulo foi clicado ");
+}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();
+  var form = document.querySelector("#form-adiciona");
+  
+  var nome = form.nome.value;
+  
+  var altura = form.altura
+  value;
+  
+  var peso = form.peso.value;
+  
+  var gordura = form.gordura.value;
+  
+  
+  var 
+
+});
