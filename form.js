d44789f699a51fcd4b1ed8fb92c644cd688c3e90
KelvynCipriano <133382331+KelvynCipriano@users.noreply.github.com>
2024-10-30 07:22:55 -0300

    Atualizar o form.js

--- form.js
+++ form.js
@@ -1,18 +1,50 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
   event.preventDefault();
-  var form = document.querySelector("#form-adiciona");
-  
-  var nome = form.nome.value;
-  
-  var altura = form.altura
-  value;
-  
-  var peso = form.peso.value;
-  
-  var gordura = form.gordura.value;
-  
-  
-  var 
 
+  var form = document.querySelector("#form-adiciona");
+
+  //Extraindo os dados digitados no formulário
+var paciente = obtemPacienteDoFormulario(form);
+
+
+
+ //Criando a linha e as células da tabela do novo paciente
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = paciente.nome;
+  pesoTd.textContent = paciente.peso;
+  alturaTd.textContent = paciente.altura;
+  gorduraTd.textContent = paciente.gordura;
+  imcTd.textContent = paciente.imc;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  //Aqui adicionamos a linha com todos os seus dados na tabela do "HTML";
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
 });
+
+function obtemPacienteDoFormulario(form) {
+
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value)
+  }
+
+ return paciente;
+}
