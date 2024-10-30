5603a9c99f7b974ce4afdf311bc0a6ae6e155cb1
KelvynCipriano <133382331+KelvynCipriano@users.noreply.github.com>
2024-10-30 07:24:05 -0300

    Atualizar o Calcula-Imc

--- main.js
+++ main.js
@@ -1,46 +1,44 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "joao Nutrição";
-
+titulo.textContent = "Carlos nutrição";
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0; i < 5; i ++) {
+for (var i = 0; i < 5; i++){
   console.log(pacientes[i]);
+
+  var tdPeso = pacientes[i].querySelector('.info-peso');
+  var peso = tdPeso.textContent;
+
+  var tdAltura = pacientes[i].querySelector('.info-altura');
+  var altura = tdAltura.textContent;
+
+  var tdIMC = pacientes[i].querySelector('.info-imc');
+
+  var pesoEnValido = true;
+  var alturaEnValida = true;
+
+  if(peso <= 0 || peso >= 600){
+    tdIMC.textContent = "Peso inválido ";
+    pesoEnValido = false;
+    pacientes[i].classList.add("dados-invalidos");
+    
+  }
+
+  if(altura <= 0 || altura > 2.80){
+    tdIMC.textContent = "Altura inválido ";
+    alturaEnValida = false;
+    pacientes[i].classList.add("dados-invalidos");
+  }
+
+  if (pesoEnValido && alturaEnValida){
+    var imc = calculaImc(peso, altura);
+    tdIMC.textContent = imc;
+  }
+}
+
+function calculaImc(peso, altura) {
+  var imc = 0;
   
-  var tdPeso = pacientes[i].querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
-var tdAltura = pacientes[i].querySelector(".info-altura");
-var altura = tdAltura.textContent;
- 
- var tdImc = pacientes[i].querySelector(".info-imc");
-
- 
-console.log(peso);
-console.log(altura);
-
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-
-if(peso < 0 || peso > 600) {
-   tdImc.textContent = "Peso inválido!";
-   pesoEhValido = false;
-   pacientes[i].classList.add("dados-invalidos");
-   }
- 
-if(altura < 0 || altura > 2.80) {
-  tdImc.textContent = "altura inválida!"
-  alturaEhValida = false;
-  pacientes[i].classList.add("dados-invalidos");
-}
-
-if (pesoEhValido && alturaEhValida) {
- var imc = calculaimc (peso, altura);
-  tdImc.textContent = imc.toFixed(2);
-}
-
-function calculaimc(peso, altura){
-var imc = 0
-imc = peso / (altura * altura);
-return toFixed(2);
+  imc = peso / (altura*altura);
+  
+  return imc.toFixed(2);
 }
